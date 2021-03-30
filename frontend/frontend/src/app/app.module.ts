ace766a1080d42ee7eb00ca654140ee17913a633
haugf <freddy@activetheory.net>
2021-03-30 17:23:11 -0400

    updated frontend from Angular5 to Angular11

--- frontend/frontend/src/app/app.module.ts
+++ frontend/frontend/src/app/app.module.ts
@@ -1,18 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
-import { CalculatedOutputComponent } from './calculated-output/calculated-output.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CalculatedOutputComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
