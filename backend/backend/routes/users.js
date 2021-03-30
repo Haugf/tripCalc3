05d23d3a44c671952f6ebd45a236a6eb8324de32
haugf <freddy@activetheory.net>
2021-03-30 15:11:39 -0400

    docker-compose now builds and launches angular app to localhost:4200

--- backend/backend/routes/users.js
+++ backend/backend/routes/users.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  console.log("been a while")
+  res.status(200).send(JSON.stringify({"hello":1}));
 });
 
 module.exports = router;
