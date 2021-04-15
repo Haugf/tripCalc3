8eccc7ec973289210891b103bbef1f51af143cf3
haugf <haug.freddy01@gmail.com>
2021-04-15 12:32:56 -0400

    added jest test for calculate route

---
+++ backend/backend/calculate.test.js
@@ -0,0 +1,14 @@
+const request = require('supertest')
+const app = require('./app.js')
+describe('Get Calculated Cost Endpoint', () => {
+  it('should return 422', async () => {
+    const res = await request(app)
+      .get('/calculate')
+      .send({
+        userId: 1,
+        title: 'test is cool',
+      })
+    expect(res.statusCode).toEqual(422)
+    // expect(res.body).toHaveProperty('post')
+  })
+})
