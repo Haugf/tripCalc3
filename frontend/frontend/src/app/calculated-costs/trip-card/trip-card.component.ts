75e842ebe66d14044021a238ab99b1421d84235f
haugf <haug.freddy01@gmail.com>
2021-04-24 00:41:02 -0400

    working on breaking up the UI into smaller components

--- frontend/frontend/src/app/calculated-costs/trip-card/trip-card.component.ts
+++ frontend/frontend/src/app/calculated-costs/trip-card/trip-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Trip, Trip2 } from '../../trip';
 
 @Component({
   selector: 'app-trip-card',
@@ -7,6 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TripCardComponent implements OnInit {
 
+  @Input() name! :string
+  @Input() cost! :number
+
+
+  
+  public model = {"camperOne_name": this.name, "camperOne_total": this.cost}
   constructor() { }
 
   ngOnInit(): void {
